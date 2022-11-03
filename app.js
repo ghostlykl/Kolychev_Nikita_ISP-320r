bbbe66c260eb0da2ae7130231af8ddd1e7fce29d
ghostlykl <103728751+ghostlykl@users.noreply.github.com>
2022-11-03 10:13:35 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+
 const express = require('express')
 const app = express()
 
 const host = '127.0.0.1'
 const port = 7000
 
-app.set('views', './views')
-app.set('view engine', 'pug')
+app.use((req, res, next) => {
+  req.headers['From-Middleware'] = 1
+  console.log('Node.js Middleware')
+  next()
+})
 
 app.get('/', (req, res) => {
-    res.render('home')
-  })  
-
-app.listen(port, host, function () {
-  console.log(`Server listens http://${host}:${port}`)
+  console.log(req.headers['From-Middleware'])
+  res.send('Home page')
 })
+
+app.listen(port, host, () =>
+  console.log(`Server listens http://${host}:${port}`)
+)
